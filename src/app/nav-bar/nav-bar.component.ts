e3b557ef9b8ceb2a4c5448238f6d5266353e4408
Almamy <almamyh27@gmail.com>
2023-04-09 05:34:37 +0000

    Ajout de lien dans la bar de navigation

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component,ElementRef, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
+
+  constructor(private elementRef: ElementRef) { }
+  ngOnInit(): void {
+  }
+
+  scrollToElement(): void {
+    const element = this.elementRef.nativeElement.querySelector('#about');
+    element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 }
